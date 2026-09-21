5b5e6a70a077dd20749ded4ee82da0b347e14fba
jackboakes <jackboakes98@gmail.com>
2026-09-21 21:48:30 +1000

    Add grid resizing via the button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,43 @@
 const gridContainer = document.querySelector("#container");
 
-function addTile() {
+function addTile(size) {
     let tile = document.createElement("div");
     tile.classList.add("tile");
+    tile.style.width = `${size}px`;
+    tile.style.height = `${size}px`;
+
     tile.addEventListener("mouseover", () => {
         tile.classList.add("tile-hovered");
     });
+
     gridContainer.appendChild(tile);
 }
 
-function populateGrid(tileCount) {
+function populateGrid(columns, rows) {
+    const tileCount = columns * rows;
+    const containerWidth = gridContainer.offsetWidth;
+    const size = containerWidth / columns;
     for(let i = 0; i < tileCount; i++)
     {
-        addTile();
+        addTile(size);
     }
 }
 
+const button = document.querySelector("button");
 
-populateGrid(16 * 16);
+button.addEventListener("click", () => {
+    gridContainer.textContent = "";
+
+    let columns = prompt("Enter the number of columns for your grid: ");
+    
+    if(columns > 100) {
+        columns = 100;
+    }
+
+    populateGrid(columns, columns);
+})
+
+
+populateGrid(16, 16);
 
 
